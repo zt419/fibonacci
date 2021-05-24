528bfd9f6da424c8d7f9f75b5b8c7c0a80502028
Tang Zhiqi <zt419@imperial.ac.uk>
2021-05-24 03:18:12 +0100

    .

--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -1,5 +1,12 @@
+from numbers import Integral # noqa D100
+
+
 def fib(n):
-    """Return the n-th Fibonacci number"""
+    """Return the n-th Fibonacci number."""
+    if not isinstance(n, Integral):
+        raise TypeError(
+            f"fib expects an integer, not a {type(n).__name__}")
+
     if n == 0:
         return 0
     elif n == 1:
